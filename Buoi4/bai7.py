99b2ec966ec9b46ff42ec64244bc5ba6354c8be1
TranPhungHung <146452599+TranPhungHung@users.noreply.github.com>
2023-11-24 12:44:44 +0700

    Update bai7.py

--- Buoi4/bai7.py
+++ Buoi4/bai7.py
@@ -1,36 +1,18 @@
 import cv2
-import tkinter as tk
-from tkinter import filedialog
 from PIL import ImageTk, Image
+import matplotlib.pyplot as plt
 
-# Định nghĩa hàm làm mịn ảnh
-def apply_smoothing(image_path):
-    image = cv2.imread(image_path)
-    smoothed_image = cv2.GaussianBlur(image, (5, 5), 0)  # Có thể thay đổi kích thước kernel và độ mờ tùy ý
-    return smoothed_image
 
-# Xử lý sự kiện khi người dùng nhấn nút "Làm mịn"
-def smooth_image():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        smoothed_image = apply_smoothing(file_path)
-        smoothed_image = cv2.cvtColor(smoothed_image, cv2.COLOR_BGR2RGB)
-        smoothed_image = Image.fromarray(smoothed_image)
-        smoothed_image = smoothed_image.resize((400, 400), Image.ANTIALIAS)
-        smoothed_image = ImageTk.PhotoImage(smoothed_image)
-        image_label.configure(image=smoothed_image)
-        image_label.image = smoothed_image
+image = cv2.imread('/content/anh1.jpg')
+smoothed_image = cv2.GaussianBlur(image, (5, 5), 3)  # Có thể thay đổi kích thước kernel và độ mờ tùy ý
+   
 
-# Tạo giao diện người dùng
-window = tk.Tk()
-window.title("Ứng dụng Làm mịn ảnh")
+# Tạo một figure với kích thước mới (width, height)
+plt.figure(figsize=(8, 8))
 
-# Tạo nút "Chọn ảnh"
-select_button = tk.Button(window, text="Chọn ảnh", command=smooth_image)
-select_button.pack(pady=10)
+plt.subplot(121),plt.imshow(image)
+plt.title('Original'),plt.xticks([]),plt.yticks([])
+plt.subplot(122),plt.imshow(smoothed_image)
+plt.title('Smoothed'),plt.xticks([]),plt.yticks([])
 
-# Tạo nhãn để hiển thị ảnh
-image_label = tk.Label(window)
-image_label.pack()
-
-window.mainloop()
+plt.show()
