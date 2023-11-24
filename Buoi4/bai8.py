9e69d8aafe6c2dd8caa0f3e1c317bbfd9c9161d8
TranPhungHung <146452599+TranPhungHung@users.noreply.github.com>
2023-11-24 12:48:07 +0700

    Update bai8.py

--- Buoi4/bai8.py
+++ Buoi4/bai8.py
@@ -1,36 +1,17 @@
 import cv2
-import numpy as np
-import tkinter as tk
-from tkinter import filedialog
-from PIL import ImageTk, Image
+import matplotlib.pyplot as plt
 
-# Định nghĩa hàm tách biên ảnh
-def apply_edge_detection(image_path):
-    image = cv2.imread(image_path, 0)
-    edges = cv2.Canny(image, 100, 200)  # Thay đổi ngưỡng tùy ý
-    return edges
 
-# Xử lý sự kiện khi người dùng nhấn nút "Tách biên"
-def detect_edges():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        edges = apply_edge_detection(file_path)
-        edges_image = Image.fromarray(edges)
-        edges_image = edges_image.resize((400, 400), Image.ANTIALIAS)
-        edges_image = ImageTk.PhotoImage(edges_image)
-        image_label.configure(image=edges_image)
-        image_label.image = edges_image
+image = cv2.imread('/content/anh1.jpg')
+edges = cv2.Canny(image, 100, 200)  # Thay đổi ngưỡng tùy ý
 
-# Tạo giao diện người dùng
-window = tk.Tk()
-window.title("Ứng dụng Tách biên ảnh")
 
-# Tạo nút "Chọn ảnh"
-select_button = tk.Button(window, text="Chọn ảnh", command=detect_edges)
-select_button.pack(pady=10)
+# Tạo một figure với kích thước mới (width, height)
+plt.figure(figsize=(8, 8))
 
-# Tạo nhãn để hiển thị ảnh
-image_label = tk.Label(window)
-image_label.pack()
+plt.subplot(121),plt.imshow(image)
+plt.title('Original'),plt.xticks([]),plt.yticks([])
+plt.subplot(122),plt.imshow(edges)
+plt.title('Edge'),plt.xticks([]),plt.yticks([])
 
-window.mainloop()
+plt.show()
